35417ebfa307c4b73b22c86fec24886861b41b18
Gustavo Pedraza <gpedraza@hermescorp.io>
2022-10-02 10:13:12 -0500

    Creating a User DTO and returning the token

--- AppCitas/Controllers/AccountController.cs
+++ AppCitas/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using AppCitas.Data;
 using AppCitas.DTOs;
 using AppCitas.Entities;
+using AppCitas.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
@@ -11,12 +12,14 @@ namespace AppCitas.Controllers
     public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
-        public AccountController(DataContext context)
+        private readonly ITokenService _tokenService;
+        public AccountController(DataContext context, ITokenService tokenService)
         {
             _context = context;
+            _tokenService = tokenService;
         }
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if(await UserExists(registerDto.Username))
                 return BadRequest("Username is already taken!");
@@ -33,11 +36,15 @@ namespace AppCitas.Controllers
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return user;
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
             if (user == null) return Unauthorized("Invalid username or password");
@@ -50,7 +57,11 @@ namespace AppCitas.Controllers
                 if (computedHash[i] != user.PasswordHash[i])
                     return Unauthorized("Invalid username or password");
             }
-            return user;
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
         #region Private methods
