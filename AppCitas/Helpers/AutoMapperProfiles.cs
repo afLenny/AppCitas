f99430489556b627f0983b7e01f25780893cd930
Gustavo Pedraza <gpedraza@hermescorp.io>
2022-10-02 14:58:58 -0500

    Configuring AutoMapper

--- AppCitas/Helpers/AutoMapperProfiles.cs
+++ AppCitas/Helpers/AutoMapperProfiles.cs
@@ -8,7 +8,10 @@ namespace AppCitas.Helpers
     {
         public AutoMapperProfiles()
         {
-            CreateMap<AppUser, MemberDto>();
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(
+                        dest => dest.PhotoUrl,
+                        opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url));
             CreateMap<Photo, PhotoDto>();
         }
     }
