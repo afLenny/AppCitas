da6c7e94321050f21eddc51dbfce75935fb566a1
Gustavo Pedraza <gpedraza@hermescorp.io>
2022-10-02 14:10:24 -0500

    Creating a repository

---
+++ AppCitas/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using AppCitas.Entities;
+
+namespace AppCitas.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<AppUser> GetUserByIdAsync(int id);
+        Task<AppUser> GetUserByUsernameAsync(string username);
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+        Task<bool> SaveAllAsync();
+        void Update(AppUser user);
+    }
+}
