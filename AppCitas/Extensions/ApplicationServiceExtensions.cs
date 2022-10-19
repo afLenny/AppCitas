17d775a723fd6d0913db1336d2080b7ccb721762
Lenny <bsegundopm@gmail.com>
2022-10-19 13:08:23 -0500

    Configuring cloudinary in the API

--- AppCitas/Extensions/ApplicationServiceExtensions.cs
+++ AppCitas/Extensions/ApplicationServiceExtensions.cs
@@ -11,6 +11,7 @@ namespace AppCitas.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
