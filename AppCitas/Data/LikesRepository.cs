ea30d55b274838fca754b0161f930c0e75a3c0d4
Lenny <bsegundopm@gmail.com>
2022-11-06 11:28:58 -0600

    Adding likes repository

---
+++ AppCitas/Data/LikesRepository.cs
@@ -0,0 +1,34 @@
+﻿using AppCitas.DTOs;
+using AppCitas.Entities;
+using AppCitas.Interfaces;
+using AutoMapper;
+
+namespace AppCitas.Data
+{
+    public class LikesRepository : ILikesRepository
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public LikesRepository(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public Task<UserLike> GetUserLike(int sourceUserId, int likedUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AppUser> GetUserWithLikes(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AppCitas/Interfaces/ILikesRepository.cs
@@ -0,0 +1,12 @@
+﻿using AppCitas.DTOs;
+using AppCitas.Entities;
+
+namespace AppCitas.Interfaces
+{
+    public interface ILikesRepository
+    {
+        Task<UserLike> GetUserLike(int sourceUserId, int likedUserId);
+        Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId);
+        Task<AppUser> GetUserWithLikes(int userId);
+    }
+}
